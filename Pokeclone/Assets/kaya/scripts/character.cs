765d5e8e6eb8f00f01ac317beaded9189a95086b
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2022-09-20 13:46:50 +0200

    scriptupate

--- Pokeclone/Assets/kaya/scripts/character.cs
+++ Pokeclone/Assets/kaya/scripts/character.cs
@@ -8,14 +8,20 @@ public class character : MonoBehaviour
     private CharacterController characterController;
 
     public float Speed = 5f;
-    // Start is called before the first frame update
+
+
+
+
+
     void Start()
     {
         Cursor.visible = false;
         characterController = GetComponent<CharacterController>();
+
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -23,16 +29,9 @@ public class character : MonoBehaviour
         characterController.Move(move * Time.deltaTime*Speed);
 
 
-
-        //grassdetect
-
-
-
-
-
     }
 
-    #region test
+    #region detect
 
 
 
