c98ea711670d159ea5ea29895f25d661cb3a7995
dejan20 <ddhaaijer@st.noorderpoort.nl>
2022-10-28 15:43:52 +0200

    tijdelijk update

---
+++ Pokeclone/Assets/Dejan/scripts/pauseMenu.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPaused = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                //Resume();
+            }
+            else
+            {
+               // pause();
+            }
+        }
+    }
+}
