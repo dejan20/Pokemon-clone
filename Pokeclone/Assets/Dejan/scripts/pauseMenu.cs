d91fe52432cd90abf696a80d137221e783b90815
dejan20 <ddhaaijer@st.noorderpoort.nl>
2022-11-04 15:45:24 +0100

    menu / hud update

--- Pokeclone/Assets/Dejan/scripts/pauseMenu.cs
+++ Pokeclone/Assets/Dejan/scripts/pauseMenu.cs
@@ -5,22 +5,66 @@ using UnityEngine;
 public class pauseMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static bool GameIsPaused = false;
+    public static bool GameIsPaused = false;   
+    public static bool inventoryIsPaused = false;
 
+    public GameObject inventoryMenuUI;
+    public GameObject pauseMenuUI;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (inventoryIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                inventorymenu();
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
             {
-                //Resume();
+                Resume();
             }
             else
             {
-               // pause();
+               Pause();
             }
         }
+
+    }
+
+    void Resume ()
+    {
+        pauseMenuUI.SetActive(false);
+        inventoryMenuUI.SetActive(false);
+        inventoryIsPaused = false;
+        GameIsPaused = false;
+
+        Time.timeScale = 1f;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        inventoryMenuUI.SetActive(false);
+        inventoryIsPaused = false;
+    }
+
+    void inventorymenu()
+    {
+            inventoryMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            inventoryIsPaused = true;
+        GameIsPaused = false;
+        pauseMenuUI.SetActive(false);
     }
 }
