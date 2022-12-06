05665aa5b23282c63e8988f07679aecf52f14881
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2022-12-06 14:38:39 +0100

    invworking

--- Pokeclone/Assets/Main/Scripts/Storm/IInteractable.cs
+++ Pokeclone/Assets/Main/Scripts/Storm/IInteractable.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public interface IInteractable
 {
+    
     void Interact(character player);
+    
+
+
 }
--- Pokeclone/Assets/kaya/scripts/INventory.cs
+++ Pokeclone/Assets/kaya/scripts/INventory.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
 
-public class INventory : MonoBehaviour
+
+public class INventory : MonoBehaviour 
 {
     public int INVsize;
     public GameObject item;
-    
+    public GameObject itemPickup;
+    public iteminteract itemInteract;
+
+    private float distance;
+    private float minimumDistance = 2f;
+
+
+    public iteminteract Interactable { get; set; }
+
     [SerializeField] List<GameObject> ItemsList;
     public void Inv()
     {
@@ -17,27 +28,57 @@ public class INventory : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-        
         ItemsList = new List<GameObject>();
 
-
+        itemPickup = GameObject.FindWithTag("Item");
 
     }
+    private void Awake()
+    {
+    }
 
-  
+
     void Update()
     {
+   
+
+
         if (Input.GetKeyDown(KeyCode.I))
         {
             ItemsList.Add((GameObject)(item));
-            
+            AddItem();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger");
+
+        if (other.CompareTag("Item"))
+        {
+            distance = Vector3.Distance(itemPickup.transform.position, transform.position);
+            Debug.Log("Work1?");
+
+            if (distance <= minimumDistance)
+            {
+                Debug.Log("Work?");
+                item = itemPickup;
+                ItemsList.Add(item);
+                AddItem();
+            }
+
+
+        }
+    }
+
+    private void AddItem()
+    {
+        
             if (ItemsList.Count > INVsize)
             {
                 Debug.Log("bigger than MAX SIZE");
                 ItemsList.RemoveAt(INVsize);
             }
-        }
-
     }
 }
