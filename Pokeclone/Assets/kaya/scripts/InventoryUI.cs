15be1c8b007e23c22d2b2313a599e6f9f544c1a8
kaya0050 <90755317+kaya0050@users.noreply.github.com>
2022-12-19 15:03:37 +0100

    invuiworking

--- Pokeclone/Assets/kaya/scripts/InventoryUI.cs
+++ Pokeclone/Assets/kaya/scripts/InventoryUI.cs
@@ -6,13 +6,27 @@ public class InventoryUI : MonoBehaviour
 {
     // Reference to the inventory script
     public INventory inventory;
-
+    public Canvas CanvasObject;   
     // Reference to the UI text element where we will display the inventory
     public TMP_Text inventoryText;
-    
-    
+
+    private void Start()
+    {
+        
+    }
     void Update()
     {
+        
+        if (Input.GetKeyDown("i")&& CanvasObject.GetComponent<Canvas>().enabled == true)
+        {
+            CanvasObject.GetComponent<Canvas>().enabled = false;
+        }
+        else if (Input.GetKeyDown("i") && CanvasObject.GetComponent<Canvas>().enabled == false)
+        {
+            CanvasObject.GetComponent<Canvas>().enabled = true;
+        }
+
+        
         // Build the string that will be displayed in the UI
         string text = "Inventory: \n";
         for (int i = 0; i < inventory.currentsize; i++)
