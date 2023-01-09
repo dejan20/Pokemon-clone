a82b3621db9324bfe691467feaeb52e084f0e41a
Stormoviek <stormhenkdehaan@gmail.com>
2023-01-09 15:10:46 +0100

    Selector

--- Pokeclone/Assets/Storm's mappie/Scripts/SpiritParty.cs
+++ Pokeclone/Assets/Storm's mappie/Scripts/SpiritParty.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpiritParty : MonoBehaviour
 {
     [SerializeField] public List<GameObject> spiritList  = new List<GameObject>(5);
     [SerializeField] private master master;
     [SerializeField] private SpiritPartyUI spiritPartyUI;
+
+    [SerializeField] private GameObject spiritSelector1;
+    [SerializeField] private GameObject spiritSelector2;
+    [SerializeField] private GameObject spiritSelector3;
+    [SerializeField] private GameObject spiritSelector4;
+    [SerializeField] private GameObject spiritSelector5;
+
     public static int i;
     public int selectedSpirit;
 
@@ -21,25 +29,70 @@ public class SpiritParty : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.F))
         {
-            Debug.Log("You added a Spirit");
             spiritList.Add(master.allSpiritList[1]);
             spiritPartyUI.SpiritPartyImages(i);
             i++;
         }
+
         if (spiritList.Count > 5)
         {
             Debug.Log("Can't have more than 5 spirits!");
             spiritList.RemoveAt(5);
         }
+
         if (Input.GetKeyDown(KeyCode.G))
         {
-            Debug.Log("Switching Spirit");
             selectedSpirit++;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            selectedSpirit--;
+        }
+
+        if (selectedSpirit == 0)
+        {
+            SelecterOff();
+
+            spiritSelector1.SetActive(true);
+        }
+        else if (selectedSpirit == 1)
+        {
+            SelecterOff();
+
+            spiritSelector2.SetActive(true);
+        }
+        else if (selectedSpirit == 2)
+        {
+            SelecterOff();
+
+            spiritSelector3.SetActive(true);
+        }
+        else if (selectedSpirit == 3)
+        {
+            SelecterOff();
+
+            spiritSelector4.SetActive(true);
+        }
+        else if (selectedSpirit == 4)
+        {
+            SelecterOff();
+
+            spiritSelector5.SetActive(true);
         }
     }
 
     public void IPlus()
     {
         i++;
+    }
+
+    public void SelecterOff()
+    {
+        spiritSelector1.SetActive(false);
+        spiritSelector2.SetActive(false);
+        spiritSelector3.SetActive(false);
+        spiritSelector4.SetActive(false);
+        spiritSelector5.SetActive(false);
     }
 }
