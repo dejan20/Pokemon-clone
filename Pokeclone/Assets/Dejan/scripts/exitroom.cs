c00170bc99901a36a7749d9b25be5aec2859dacd
dejan20 <ddhaaijer@st.noorderpoort.nl>
2023-01-27 12:12:39 +0100

    water level update

---
+++ Pokeclone/Assets/Dejan/scripts/exitroom.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class exitroom : MonoBehaviour
+{
+    void OnTriggerEnter(Collider ChangeScene)
+    {
+        if (ChangeScene.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevelAdditive(1);
+        }
+    }
+}
--- Pokeclone/Assets/Dejan/scripts/platfromrise.cs
+++ Pokeclone/Assets/Dejan/scripts/platfromrise.cs
@@ -7,12 +7,13 @@ public class platfromrise : MonoBehaviour
     public Vector3 StartPosition;
     public Vector3 EndPosition1 = new Vector3(2.45099998f, 1.27600002f, 1.82700002f);
     public Vector3 CurrentPosition;
-    public GameObject platform;
+    public GameObject platform1;
+
 
     bool SwitchActivated = false;
     private void Awake()
     {
-        CurrentPosition = gameObject.transform.position;
+        CurrentPosition = platform1.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -29,7 +30,7 @@ public class platfromrise : MonoBehaviour
         if (SwitchActivated)
         {
             CurrentPosition = Vector3.Lerp(CurrentPosition, EndPosition1, Time.deltaTime);
-            gameObject.transform.position = CurrentPosition;
+            platform1.transform.position = CurrentPosition;
         }
     }
 }
