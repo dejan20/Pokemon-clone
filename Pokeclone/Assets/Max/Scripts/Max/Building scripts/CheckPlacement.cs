cc4e47b39c644a1f4a7b97b375fae24c11b681b4
max <maxquirijns@gmail.com>
2023-02-03 07:34:56 +0100

    building system fix

--- Pokeclone/Assets/Max/Scripts/Max/Building scripts/CheckPlacement.cs
+++ Pokeclone/Assets/Max/Scripts/Max/Building scripts/CheckPlacement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CheckPlacement : MonoBehaviour
+public class CheckPlacement : BuildingManager
 {
     BuildingManager buildingManager;
 
